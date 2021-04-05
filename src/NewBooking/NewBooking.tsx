00846360dcf6b3a1913a663d6c8580b87ea6a9aa
T_Yokoi_AIOT <yokoi@ai-ot.com>
2021-04-05 19:19:28 +0900

    added FullCalendar to show bookings

--- src/NewBooking/NewBooking.tsx
+++ src/NewBooking/NewBooking.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import BookingCalendar from './BookingCalendar'
 
 const NewBooking: React.FC = () => {
   return (
     <div className="NewBooking">
       <p>NewBooking</p>
+      <BookingCalendar />
     </div>
   )
 }
