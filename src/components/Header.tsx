288ad561b365f60017c70bf91906225d202e80c8
T_Yokoi_AIOT <yokoi@ai-ot.com>
2021-04-07 00:06:16 +0900

    Added sidebar menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import { Button, Drawer, List, ListItem, ListItemText } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import { logoutThroughFirebase } from '../redux/userRedux/userThunk'
 
 interface HeaderProps {
   title: string
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
+  const dispatch = useDispatch()
+  const [showSidebarMenu, setShowSidebarMenu] = useState<boolean>(false)
+
   return (
     <div className="header">
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton edge="start" className="header.menuButton" color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className="header.menuButton"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => {
+              setShowSidebarMenu(true)
+            }}
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit">
             {title}
           </Typography>
         </Toolbar>
+        <Drawer style={{ width: '30%' }} anchor="left" open={showSidebarMenu}>
+          <List>
+            <ListItem>
+              <ListItemText>Test</ListItemText>
+            </ListItem>
+            <ListItem>
+              <Button
+                onClick={() => {
+                  setShowSidebarMenu(false)
+                }}
+              >
+                Close the sidebar menu
+              </Button>
+            </ListItem>
+            <ListItem>
+              <Button
+                onClick={() => {
+                  dispatch(logoutThroughFirebase())
+                }}
+              >
+                Logout
+              </Button>
+            </ListItem>
+          </List>
+        </Drawer>
       </AppBar>
     </div>
   )
