01ebfd25ccd05d5311277ff07ae434b072aaaef1
TKYK93 <laylalayla2905@gmail.com>
2021-04-15 13:16:30 +0900

    changed colors of the entire app

--- src/App.tsx
+++ src/App.tsx
@@ -14,32 +14,52 @@ import AvailableMachines from './pages/Setting/AvailableMachines'
 import AddMachines from './pages/Setting/AddMachines'
 import GroupId from './pages/Setting/GroupId'
 import AccountUsers from './pages/Setting/AccountUsers'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+import { myColors } from './config'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: myColors.myColor3,
+    },
+    secondary: {
+      main: myColors.myColor5,
+    },
+  },
+})
 
 const App: React.FC = () => {
   const isAuthenticated = useSelector((state: AppState) => state.userState.loginUser.isAuthenticated)
 
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Redirect exact from="/" to="/login" />
-          <PublicRoute exact path={'/login'} component={Login} isAuthenticated={isAuthenticated} />
-          <Route exact path={'/signUp'} component={SignUp}></Route>
-          <PrivateRoute exact path={'/bookingList'} component={BookingList} isAuthenticated={isAuthenticated} />
-          <PrivateRoute exact path={'/newBooking'} isAuthenticated={isAuthenticated} component={NewBooking} />
-          <PrivateRoute exact path={'/setting'} isAuthenticated={isAuthenticated} component={Setting} />
-          <PrivateRoute
-            exact
-            path={'/setting/allMachines'}
-            isAuthenticated={isAuthenticated}
-            component={AvailableMachines}
-          />
-          <PrivateRoute exact path={'/setting/addMachines'} isAuthenticated={isAuthenticated} component={AddMachines} />
-          <PrivateRoute exact path={'/setting/groupId'} isAuthenticated={isAuthenticated} component={GroupId} />
-          <PrivateRoute exact path={'/setting/users'} isAuthenticated={isAuthenticated} component={AccountUsers} />
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div className="app">
+          <Switch>
+            <Redirect exact from="/" to="/login" />
+            <PublicRoute exact path={'/login'} component={Login} isAuthenticated={isAuthenticated} />
+            <Route exact path={'/signUp'} component={SignUp}></Route>
+            <PrivateRoute exact path={'/bookingList'} component={BookingList} isAuthenticated={isAuthenticated} />
+            <PrivateRoute exact path={'/newBooking'} isAuthenticated={isAuthenticated} component={NewBooking} />
+            <PrivateRoute exact path={'/setting'} isAuthenticated={isAuthenticated} component={Setting} />
+            <PrivateRoute
+              exact
+              path={'/setting/allMachines'}
+              isAuthenticated={isAuthenticated}
+              component={AvailableMachines}
+            />
+            <PrivateRoute
+              exact
+              path={'/setting/addMachines'}
+              isAuthenticated={isAuthenticated}
+              component={AddMachines}
+            />
+            <PrivateRoute exact path={'/setting/groupId'} isAuthenticated={isAuthenticated} component={GroupId} />
+            <PrivateRoute exact path={'/setting/users'} isAuthenticated={isAuthenticated} component={AccountUsers} />
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   )
 }
 
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const myColors = {
+  white: '#fff',
+  black: '#000',
+  myColor1: '#B4B8AB', // Ash Gray
+  myColor2: '#153243', // Gunmetal
+  myColor3: '#284B63', // Indigo Dye
+  myColor4: '#F4F9E9', //Ivory
+  myColor5: '#ff6700', //Safety Orange Blaze Orange
+}
+
+export type ColorName = keyof typeof myColors
