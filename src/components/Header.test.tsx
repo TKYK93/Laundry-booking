b7915784b46552de179ffce6cdd1c061cca01d95
TKYK93 <laylalayla2905@gmail.com>
2021-04-17 12:47:25 +0900

    added test (react-testing-library)

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+
+const title = "Header Test"
+
+describe('Header test', () => {
+    test('renders Header Component and test if the text appears', () => {
+      render(<Provider store={store}><Header title={title}/></Provider>);
+      screen.debug()
+      expect(screen.getByText(title)).toBeInTheDocument();
+      
+    });
+  });
