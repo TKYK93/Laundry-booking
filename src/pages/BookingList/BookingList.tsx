1f5d7580735f5a2ceec170e3e7d608211ddd4432
TKYK93 <laylalayla2905@gmail.com>
2021-04-17 14:05:18 +0900

    added loading progress icon

--- src/pages/BookingList/BookingList.tsx
+++ src/pages/BookingList/BookingList.tsx
@@ -1,5 +1,5 @@
-import { List, makeStyles } from '@material-ui/core'
-import React, { useEffect } from 'react'
+import { List } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Header from '../../components/Header'
 import { getPersonalBookingFromFirebase } from '../../redux/BookingRedux/bookingThunk'
@@ -7,29 +7,38 @@ import { AppState } from '../../redux/store'
 import BookingListItem from './BookingListItem'
 import { db } from '../../firebase'
 import { getMachinesFromFirebase } from '../../redux/machineRedux/machineThunk'
+import LoadingProgress from '../../components/Progress'
 
 const BookingList: React.FC = () => {
   const dispatch = useDispatch()
   const currBookingList = useSelector((state: AppState) => state.bookingState.personalBookings)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
   useEffect(() => {
     dispatch(getMachinesFromFirebase())
     const doc = db.collection('bookings')
-    const unsubscribe = doc.onSnapshot(() => {
-      dispatch(getPersonalBookingFromFirebase())
+    const unsubscribe = doc.onSnapshot(async () => {
+      await setIsLoading(true)
+      await dispatch(getPersonalBookingFromFirebase())
+      await setIsLoading(false)
     })
     return () => unsubscribe()
   }, [])
 
   return (
-    <div className={"bookingList"}>
+    <div className={'bookingList'}>
       <Header title={'BookingList'} />
-      <List>
-        {currBookingList.length >= 1 ? (
-          currBookingList.map((item, index) => <BookingListItem {...item} key={`bookingItem${index}`} />)
-        ) : (
-          <p>You have no booking.</p>
-        )}
-      </List>
+      {isLoading ? (
+        <LoadingProgress />
+      ) : (
+        <List>
+          {currBookingList.length >= 1 ? (
+            currBookingList.map((item, index) => <BookingListItem {...item} key={`bookingItem${index}`} />)
+          ) : (
+            <p>You have no booking.</p>
+          )}
+        </List>
+      )}
     </div>
   )
 }
